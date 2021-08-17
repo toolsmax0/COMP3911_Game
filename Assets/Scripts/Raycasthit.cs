74231775903ea752cdc764bd3c4ab325b43f5c9b
ChauChunHong <74500413+ChauChunHong@users.noreply.github.com>
2021-08-17 21:40:32 +0800

    update raycast for store inside, add new item, tag

--- Assets/Scripts/Raycasthit.cs
+++ Assets/Scripts/Raycasthit.cs
@@ -11,8 +11,28 @@ public class Raycasthit : MonoBehaviour
     private const float _maxDistance = 5000;
     private Animator animator;
     private bool opened = false;
+    GameObject [] scene_0;
+    GameObject [] scene_1  ;
+    GameObject [] scene_2;
 
-
+    void Start()
+    {
+        scene_0 = GameObject.FindGameObjectsWithTag("SceneSetting_0");
+        scene_1 = GameObject.FindGameObjectsWithTag("SceneSetting_1");
+        scene_2 = GameObject.FindGameObjectsWithTag("SceneSetting_2");
+        foreach (GameObject scene0 in scene_0)
+        {
+            scene0.SetActive(true);
+        }
+        foreach (GameObject scene1 in scene_1)
+        {
+            scene1.SetActive(true);
+        }
+        foreach (GameObject scene2 in scene_2)
+        {
+            scene2.SetActive(true);
+        }
+    }
 
     void Update()
     {
@@ -25,6 +45,55 @@ public class Raycasthit : MonoBehaviour
                 {
                     opened = true;
                     door.OpenDoor();
+                    int randomvalue = Random.Range(0, 2);
+                    switch (randomvalue)
+                    {
+
+                        case 0:
+                            foreach (GameObject scene0 in scene_0)
+                            {
+                                scene0.SetActive(true);
+                            }
+                            foreach (GameObject scene1 in scene_1)
+                            {
+                                scene1.SetActive(false);
+                            }
+                            foreach (GameObject scene2 in scene_2)
+                            {
+                                scene2.SetActive(false);
+                            }
+                            break;
+
+                        case 1:
+                            foreach (GameObject scene1 in scene_1)
+                            {
+                                scene1.SetActive(true);
+                            }
+                            foreach (GameObject scene0 in scene_0)
+                            {
+                                scene0.SetActive(false);
+                            }
+                            foreach (GameObject scene2 in scene_2)
+                            {
+                                scene2.SetActive(false);
+                            }
+                            break;
+
+                        case 2:
+                            foreach (GameObject scene2 in scene_2)
+                            {
+                                scene2.SetActive(true);
+                            }
+                            foreach (GameObject scene1 in scene_1)
+                            {
+                                scene1.SetActive(false);
+                            }
+                            foreach (GameObject scene0 in scene_0)
+                            {
+                                scene0.SetActive(false);
+                            }
+                            break;
+                    }
                 }
                 else
                 {
