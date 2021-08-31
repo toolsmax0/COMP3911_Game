15b784ca1b2dc66365b46413d9b02ed2af041c76
toolsmax0 <xingshijichina@outlook.com>
2021-08-31 22:16:43 +0800

    font fixed & bag count update

---
+++ Assets/Scripts/Raw/UI/Bag.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Bag : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BagPanel.numBag=UnityEngine.Random.Range(20,31);
+        gameObject.GetComponent<TMP_Text>().text="一共需要"+BagPanel.numBag+"个垃圾袋.";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/UIFramework/Concrete/BagPanel.cs
+++ Assets/Scripts/UIFramework/Concrete/BagPanel.cs
@@ -12,47 +12,53 @@ public class BagPanel : BasePanel
 {
     static readonly string path = "Prefabs/UI/Panel/BagPanel";
 
-    public BagPanel() : base(new UIType(path)) { }
+    public static int numBag;
+
+    public BagPanel() :
+        base(new UIType(path))
+    {
+    }
 
     public override void OnEnter()
     {
-        UITool.GetOrAddComponentInChildren<Button>("BtnBack").onClick.AddListener(() =>
-        {
-            CountBag.resetPanel();
-            PanelManager.Pop();
-        });
+        UITool
+            .GetOrAddComponentInChildren<Button>("BtnBack")
+            .onClick
+            .AddListener(() =>
+            {
+                CountBag.resetPanel();
+                PanelManager.Pop();
+            });
 
         UITool
-        .GetOrAddComponentInChildren<TMP_InputField>("InputField1")
-        .onValueChanged
-        .AddListener((s) =>
-        {
-            Debug.Log(1);
-
-            // UITool
-            //     .GetOrAddComponentInChildren<TMP_InputField>("InputField1")
-            //     .GetComponent<TMP_Text>()
-            //     .text = "";
-            int inputed = System.Int32.Parse(s);
-
-            //todo: please provide interface to get the number of bag in scene
-            // int presented = ObjectRefresh.GetNumOfBrokenBottles();
-            int presented = ObjectRefresh.GetNumOfBags();
-            Debug.Log("n==" + inputed + " m==" + presented);
-            if (inputed == presented)
+            .GetOrAddComponentInChildren<TMP_InputField>("InputField1")
+            .onValueChanged
+            .AddListener((s) =>
             {
-                UITool
-                   .GetOrAddComponentInChildren
-                   <Image>("InputField1")
-                   .color = UnityEngine.Color.green;
-            }
-            else
-            {
-                UITool
-                .GetOrAddComponentInChildren
-                <Image>("InputField1")
-                .color = UnityEngine.Color.red;
-            }
-        });
+                Debug.Log(1);
+
+                // UITool
+                //     .GetOrAddComponentInChildren<TMP_InputField>("InputField1")
+                //     .GetComponent<TMP_Text>()
+                //     .text = "";
+                int inputed = System.Int32.Parse(s);
+
+                //todo: please provide interface to get the number of bag in scene
+                // int presented = ObjectRefresh.GetNumOfBrokenBottles();
+                int presented = numBag - ObjectRefresh.GetNumOfBags();
+                Debug.Log("n==" + inputed + " m==" + presented);
+                if (inputed == presented)
+                {
+                    UITool
+                        .GetOrAddComponentInChildren<Image>("InputField1")
+                        .color = UnityEngine.Color.green;
+                }
+                else
+                {
+                    UITool
+                        .GetOrAddComponentInChildren<Image>("InputField1")
+                        .color = UnityEngine.Color.red;
+                }
+            });
     }
 }
